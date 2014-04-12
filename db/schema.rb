467734a50aca6045023370e07cb3a7d99fb23db7
Imran <ikhoja@fiksu.com>
2014-04-12 16:47:01 -0400

    committing lack of sqlite so that I can push to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140315220124) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contracts", force: true do |t|
     t.integer  "team_id"
     t.integer  "player_id"
@@ -67,7 +70,7 @@ ActiveRecord::Schema.define(version: 20140315220124) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
